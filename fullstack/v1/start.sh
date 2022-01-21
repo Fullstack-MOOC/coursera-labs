036d6dffe01051aa1c6c566f47fdda06eaf3d636
Tim Tregubov <tim@zingweb.com>
2022-01-21 10:48:26 -0500

    open project folder

--- fullstack/v1/start.sh
+++ fullstack/v1/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mongod -f /etc/mongod.conf &
 python3 ${VSCODE_USER}/coursera/submitButton.py
-code-server --config /tmp/config/code-server/config.yaml
+code-server --config /tmp/config/code-server/config.yaml /home/coder/project
